add69ffe62fcf82c8bc5a3a25fad4ffa2725fbbf
Bas Dingemans <478244@student.saxion.nl>
2020-04-16 18:53:40 +0200

    Removed forcerender from box Emoji

--- src/main/java/com/github/bigibas123/bigidiscordbot/util/Emoji.java
+++ src/main/java/com/github/bigibas123/bigidiscordbot/util/Emoji.java
@@ -10,17 +10,17 @@ public enum Emoji {
     WAVE("\uD83D\uDC4B"),       //👋
     PAUSE("\u23F8"),            //⏸
     PLAY("\u25B6"),             //▶
-    ONE("\u0031\uFE0F\u20E3"),  //1️⃣
-    TWO("\u0032\uFE0F\u20E3"),  //2️⃣
-    THREE("\u0033\uFE0F\u20E3"),//3️⃣
-    FOUR("\u0034\uFE0F\u20E3"), //4️⃣
-    FIVE("\u0035\uFE0F\u20E3"), //5️⃣
-    SIX("\u0036\uFE0F\u20E3"),  //6️⃣
-    SEVEN("\u0037\uFE0F\u20E3"),//7️⃣
-    EIGHT("\u0038\uFE0F\u20E3"),//8️⃣
-    NINE("\u0039\uFE0F\u20E3"), //9️⃣
+    ONE("\u0031\u20E3"),        //1️⃣
+    TWO("\u0032\u20E3"),        //2️⃣
+    THREE("\u0033\u20E3"),      //3️⃣
+    FOUR("\u0034\u20E3"),       //4️⃣
+    FIVE("\u0035\u20E3"),       //5️⃣
+    SIX("\u0036\u20E3"),        //6️⃣
+    SEVEN("\u0037\u20E3"),      //7️⃣
+    EIGHT("\u0038\u20E3"),      //8️⃣
+    NINE("\u0039\u20E3"),       //9️⃣
     TEN("\uD83D\uDD1F"),        //🔟
-    WARNING("\u26A0\uFE0F")     //⚠️    
+    WARNING("\u26A0")           //⚠️
     ;
 
     private final String toString;
