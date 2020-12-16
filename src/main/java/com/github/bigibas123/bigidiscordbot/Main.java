bc7e95d9836a974bcc228afb7f455a2e779c582a
Bas Dingemans <478244@student.saxion.nl>
2020-12-16 02:16:29 +0100

    Added missing cacheflags and gatewayIntents to settings

--- src/main/java/com/github/bigibas123/bigidiscordbot/Main.java
+++ src/main/java/com/github/bigibas123/bigidiscordbot/Main.java
@@ -16,11 +16,7 @@ import javax.security.auth.login.LoginException;
 import java.util.ArrayList;
 
 import static net.dv8tion.jda.api.requests.GatewayIntent.*;
-import static net.dv8tion.jda.api.utils.cache.CacheFlag.ACTIVITY;
-import static net.dv8tion.jda.api.utils.cache.CacheFlag.CLIENT_STATUS;
-import static net.dv8tion.jda.api.utils.cache.CacheFlag.EMOTE;
-import static net.dv8tion.jda.api.utils.cache.CacheFlag.MEMBER_OVERRIDES;
-import static net.dv8tion.jda.api.utils.cache.CacheFlag.VOICE_STATE;
+import static net.dv8tion.jda.api.utils.cache.CacheFlag.*;
 
 public class Main {
 
@@ -36,10 +32,10 @@ public class Main {
         soundManager = new SoundManager();
         ShardManager b = DefaultShardManagerBuilder.create(Reference.token, new ArrayList<>())
                 .enableIntents(GUILD_VOICE_STATES, GUILD_MESSAGES, GUILD_MESSAGE_REACTIONS, DIRECT_MESSAGES, DIRECT_MESSAGE_REACTIONS)
-                .disableIntents(GUILD_MEMBERS, GUILD_BANS, GUILD_EMOJIS, GUILD_INVITES, GUILD_PRESENCES, GUILD_MESSAGE_TYPING, DIRECT_MESSAGE_TYPING)
+                .disableIntents(GUILD_MEMBERS, GUILD_BANS, GUILD_EMOJIS,GUILD_WEBHOOKS, GUILD_INVITES, GUILD_PRESENCES, GUILD_MESSAGE_TYPING, DIRECT_MESSAGE_TYPING)
                 .addEventListeners(new Listener())
-                .enableCache(MEMBER_OVERRIDES)
-                .disableCache(ACTIVITY, EMOTE, CLIENT_STATUS)
+                .enableCache(MEMBER_OVERRIDES,ROLE_TAGS)
+                .disableCache(ACTIVITY, VOICE_STATE,EMOTE, CLIENT_STATUS )
                 .setActivity(null)
                 .setAudioSendFactory(new NativeAudioSendFactory())
                 .setAutoReconnect(true)
@@ -58,37 +54,5 @@ public class Main {
                 .setStatus(OnlineStatus.ONLINE)
                 .setToken(Reference.token)
                 .build();
-        /*
-        jda = JDABuilder.create(Reference.token, new ArrayList<>())
-                .enableIntents(GUILD_VOICE_STATES,GUILD_MESSAGES,GUILD_MESSAGE_REACTIONS,DIRECT_MESSAGES,DIRECT_MESSAGE_REACTIONS)
-                .disableIntents(GUILD_MEMBERS,GUILD_BANS,GUILD_EMOJIS,GUILD_INVITES,GUILD_PRESENCES,GUILD_MESSAGE_TYPING,DIRECT_MESSAGE_TYPING)
-                .addEventListeners(new Listener())
-                .enableCache(EMOTE,MEMBER_OVERRIDES)
-                .disableCache(ACTIVITY,VOICE_STATE,CLIENT_STATUS)
-                .setActivity(null)
-                .setAudioSendFactory(DefaultSendSystem::new)
-                .setAutoReconnect(true)
-                .setBulkDeleteSplittingEnabled(true)
-                .setChunkingFilter(ChunkingFilter.NONE)
-                .setCompression(Compression.ZLIB)
-                .setContextEnabled(true)
-                .setEnableShutdownHook(true)
-                .setLargeThreshold(50)
-                .setMaxBufferSize(4096)
-                .setMaxReconnectDelay(64)
-                .setMemberCachePolicy(MemberCachePolicy.DEFAULT)
-                .setRawEventsEnabled(false)
-                .setRelativeRateLimit(true)
-                .setRequestTimeoutRetry(true)
-                .setStatus(OnlineStatus.ONLINE)
-                .setToken(Reference.token)
-                .useSharding(0,1)
-                .build();
-        try {
-            jda.awaitReady();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        */
     }
 }
