cd340a581502162ddc17ccd7ed0c4d85acf56d90
Bas Dingemans <478244@student.saxion.nl>
2020-12-22 02:47:35 +0100

    changed activity and starting message

--- src/main/java/com/github/bigibas123/bigidiscordbot/Listener.java
+++ src/main/java/com/github/bigibas123/bigidiscordbot/Listener.java
@@ -5,7 +5,7 @@ import com.github.bigibas123.bigidiscordbot.util.ReactionScheduler;
 import com.github.bigibas123.bigidiscordbot.util.Utils;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.events.*;
+import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
@@ -18,58 +18,62 @@ import java.time.LocalDateTime;
 
 public class Listener extends ListenerAdapter {
 
-    private final int shardID;
-    private CommandHandling handling;
+	private final int shardID;
+	private CommandHandling handling;
 
-    public Listener(int i) {
-        this.shardID = i;
-    }
+	public Listener(int i) {
+		this.shardID = i;
+	}
 
-    @Override
-    public void onReady(@NotNull ReadyEvent event) {
-        super.onReady(event);
-        event.getJDA().openPrivateChannelById(Reference.ownerID).queue(c ->
-                c.sendMessage("Started at " + LocalDateTime.now().toString()).queue()
-        );
-        String activityString = MessageFormat.format("@mention help\t| [{0}/{1}_{2}]", event.getJDA().getShardInfo().getShardId() + 1, event.getJDA().getShardInfo().getShardTotal(),this.shardID);
-        Activity act = Activity.of(Activity.ActivityType.DEFAULT, activityString);
-        event.getJDA().getPresence().setPresence(
-                OnlineStatus.ONLINE,
-                act,
-                false
-        );
-        this.handling = new CommandHandling();
-    }
+	@Override
+	public void onReady(@NotNull ReadyEvent event) {
+		super.onReady(event);
+		int siID = event.getJDA().getShardInfo().getShardId();
+		event.getJDA().openPrivateChannelById(Reference.ownerID).queue(c ->
+				c.sendMessage("Started shard:" + shardID + "_" + siID + " at " + LocalDateTime.now().toString()).queue()
+		);
+		String activityString = MessageFormat.format("@mention help\t| [{0}/{1}]",
+				siID != shardID ? (siID + 1) + "_" + (shardID + 1) : (siID + 1),
+				event.getJDA().getShardInfo().getShardTotal()
+		);
+		Activity act = Activity.of(Activity.ActivityType.DEFAULT, activityString);
+		event.getJDA().getPresence().setPresence(
+				OnlineStatus.ONLINE,
+				act,
+				false
+		);
+		this.handling = new CommandHandling();
+	}
 
-    @Override
-    public void onPrivateMessageReceived(@NotNull PrivateMessageReceivedEvent event) {
-        super.onPrivateMessageReceived(event);
-        if (!event.getAuthor().isBot()) {
-            if (Utils.mentionsMe(event.getMessage())) {
-                handling.handleCommand(event.getMessage());
-            } else {
-                MessageAction messageAction = event.getChannel().sendMessage(event.getMessage().getContentRaw().replace("i","o"));
-                messageAction.queue();
-            }
-        }
-    }
+	@Override
+	public void onPrivateMessageReceived(@NotNull PrivateMessageReceivedEvent event) {
+		super.onPrivateMessageReceived(event);
+		if (!event.getAuthor().isBot()) {
+			if (Utils.mentionsMe(event.getMessage())) {
+				handling.handleCommand(event.getMessage());
+			} else {
+				MessageAction messageAction = event.getChannel().sendMessage(event.getMessage().getContentRaw().replace("i", "o"));
+				messageAction.queue();
+			}
+		}
+	}
 
-    @Override
-    public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
-        super.onGuildMessageReceived(event);
-        if (!event.getAuthor().isBot()) {
-            if (Utils.mentionsMe(event.getMessage())) {
-                handling.handleCommand(event.getMessage());
-            }
-        }
-    }
+	@Override
+	public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
+		super.onGuildMessageReceived(event);
+		if (!event.getAuthor().isBot()) {
+			if (Utils.mentionsMe(event.getMessage())) {
+				handling.handleCommand(event.getMessage());
+			}
+		}
+	}
 
-    @Override
-    public void onMessageReactionAdd(@NotNull MessageReactionAddEvent event) {
-        super.onMessageReactionAdd(event);
-        if (event.getReaction().isSelf()) {
-            ReactionScheduler.check(event.getReaction());
-        }
-    }
+	@Override
+	public void onMessageReactionAdd(@NotNull MessageReactionAddEvent event) {
+		super.onMessageReactionAdd(event);
+		if (event.getReaction().isSelf()) {
+			ReactionScheduler.check(event.getReaction());
+		}
+	}
 
 }
